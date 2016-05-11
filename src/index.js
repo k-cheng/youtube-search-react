9a3bdee9ccdcd31a8de310b06c658370f7978b6a
Kevin Cheng <09chengk@gmail.com>
2016-05-11 15:39:29 -0700

    implements youtube search of surfboards

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar'; // need to include actual path for js files we write
 
 const API_KEY = 'AIzaSyDw3PS2mKse5HrbKdilcGpInX0JS3wiOJo'; // lets us make requests to youtube
 
-// new component
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+
+// new component - want app to keep track of videos by recording them on its state
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      //this is equivalent of this.setState({ videos: videos})
+      // can only do this when key and property have the same variable name
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
 
