ca12958e17e107a697c9d48f2030cb57942f0972
Kevin Cheng <09chengk@gmail.com>
2016-05-16 13:36:28 -0700

    gets youtube video to show up

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -1,1 +1,27 @@
-video_detail.js
+import React from 'react';
+
+const VideoDetail = (props) => {
+  if(!video) {
+    return <div>Loading...</div>;
+  }
+
+  const video = props.video;
+  const title = video.snippet.title;
+  const videoId = video.id.videoId;
+  const url = `https://www.youtube.com/embed/${videoId}`;
+    // can also write const url = `https://www.youtube.com/embed/${videoId}`
+
+  return (
+    <div className='video-detail col-md-8'>
+      <div className='embed-responsive embed-responsive-16by9'>
+        <iframe className='embed-responsive-item' src={url}></iframe>
+      </div>
+      <div className='details'>
+        <div>{title}</div>
+        <div>{video.snippet.description}</div>
+      </div>
+    </div>
+  )
+};
+
+export default VideoDetail;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar'; // need to include actual path for js files we write
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyDw3PS2mKse5HrbKdilcGpInX0JS3wiOJo'; // lets us make requests to youtube
 
@@ -25,6 +27,9 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
+      {/*this is to pass data from parent, App into VideoList component*/}
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
